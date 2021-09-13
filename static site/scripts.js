f381dc8bc2ddcea5a483190e297430677f743d0f
Jonathan McDowell <74840102+mcd-03@users.noreply.github.com>
2021-09-12 20:58:38 -0400

    Add files via upload

--- static site/scripts.js
+++ static site/scripts.js
@@ -1,11 +1,40 @@
 
-function updateTopic(event) {
-    document.getElementById("topic-name").innerHTML = event.target.innerHTML;
+// Alerts users to the menu if they don't select an equation type
+function infoChoose() {
+    let topic = document.getElementById("topic-name");
+    topic.setAttribute("class", "shake");
+    let menuIcon = document.getElementById("menu-icon");
+    menuIcon.setAttribute("class", "blur-focus");
+    let button = document.getElementById("next-button");
+    button.disabled = true;
+    setTimeout(function() {
+        topic.classList.remove("shake");
+        menuIcon.classList.remove("blur-focus");
+        button.disabled = false;
+    }, 2000);
 }
 
+// Changes the type of equation shown to the one clicked in the menu
+// Updates the gradient
+function updateTopic(event) {
+    document.getElementById("topic-name").innerHTML = event.target.innerHTML;
+    let topic = event.target.innerHTML;
+    let dispatch = {
+        "choose an equation": 'linear-gradient(60deg, #3d3393 0%, #2b76b9 37%, #2cacd1 65%, #35eb93 100%)',
+        "one-step": 'linear-gradient(to top, #30cfd0 0%, #330867 100%)',
+        "two-step": 'linear-gradient(15deg, #13547a 0%, #80d0c7 100%)',
+        "multi-step": 'linear-gradient(to top, #00c6fb 0%, #005bea 100%)',
+        "v.o.b.s.": 'linear-gradient(to top, #09203f 0%, #537895 100%)',
+    };
+    document.body.style.backgroundImage = dispatch[topic];
+}
+
+// Depending on the type of equation selected, calls a function
+// That function calls updateProblem to change the problem and answer
+// Shown on screen
 function replaceEquation(event) {
-    topic = document.getElementById("topic-name").innerHTML
-    var dispatch = {
+    let topic = document.getElementById("topic-name").innerHTML;
+    let dispatch = {
         "choose an equation": infoChoose,
         "one-step": createOneStep,
         "two-step": createTwoStep,
@@ -15,37 +44,24 @@ function replaceEquation(event) {
     dispatch[topic]();
 }
 
-function infoChoose() {
-    topic = document.getElementById("topic-name");
-    topic.setAttribute("class", "shake");
-    menuIcon = document.getElementById("menu-icon");
-    menuIcon.setAttribute("class", "blur-focus");
-    var button = document.getElementById("next-button");
-    button.disabled = true;
-    setTimeout(function() {
-        topic.classList.remove("shake");
-        menuIcon.classList.remove("blur-focus");
-        button.disabled = false;
-    }, 2000);
-}
-
+// Updates the inner HTML of the problem and answer shown
 function updateProblem(equation, answer) {
-    currentEquation = document.getElementById("current-equation");
+    let currentEquation = document.getElementById("current-equation");
     currentEquation.style.opacity = "0";
     currentEquation.innerHTML = equation;
-    setTimeout(function() {
+    let updateProblemTimer = setTimeout(function() {
         currentEquation.style.opacity = "1";
-    }, 300);
-    currentAnswer = document.getElementById("current-answer");
+    }, 200);
+    let currentAnswer = document.getElementById("current-answer");
     currentAnswer.innerHTML = answer;
     MathJax.typeset();
     closeMenu();
 }
 
-//chooses and calls from a list of functions that create one step equations
+// Randomly chooses from a list of function that create different
+// Types of one-step equations. Calls the chosen function.
 function createOneStep() {
-    document.body.style.backgroundImage = "linear-gradient(to top, #30cfd0 0%, #330867 100%)";
-    var toggle = randInt(1, 5);
+    let toggle = randInt(1, 5);
     switch (toggle) {
         case 1:
             return oneDivision();
@@ -59,41 +75,41 @@ function createOneStep() {
 }
 
 function oneDivision() {
-    var a = randInt(2,10);
-    var b = randInt(2,5);
-    var equation = `\\(${a}x = ${a*b}\\)`;
-    var answer = `\\(x=${b}\\)`;
+    let a = randInt(2,10);
+    let b = randInt(2,5);
+    let equation = `\\(${a}x = ${a*b}\\)`;
+    let answer = `\\(x=${b}\\)`;
     updateProblem(equation, answer);
 }
 
 function oneMultiplication() {
-    var a = randInt(2,10);
-    var b = randInt(-10, 11);
-    var equation = `\\(\\frac{x}{${a}} = ${b}\\)`;
-    var answer = `\\(x=${a*b}\\)`;
+    let a = randInt(2,10);
+    let b = randInt(-10, 11);
+    let equation = `\\(\\frac{x}{${a}} = ${b}\\)`;
+    let answer = `\\(x=${a*b}\\)`;
     updateProblem(equation, answer);
 }
 
 function oneSubtraction() {
-    var a = randInt(-10, 20);
-    var b = randInt(-20, 30);
-    var equation = `\\(x-${a} = ${b}\\)`;
-    var answer = `\\(x=${b+a}\\)`
+    let a = randInt(-10, 20);
+    let b = randInt(-20, 30);
+    let equation = `\\(x-${a} = ${b}\\)`;
+    let answer = `\\(x=${b+a}\\)`
     updateProblem(equation, answer);
 }
 
 function oneAddition() {
-    var a = randInt(-10, 20);
-    var b = randInt(-20, 30);
-    var equation = `\\(x+${a} = ${b}\\)`;
-    var answer = `\\(x=${b-a}\\)`
+    let a = randInt(-10, 20);
+    let b = randInt(-20, 30);
+    let equation = `\\(x+${a} = ${b}\\)`;
+    let answer = `\\(x=${b-a}\\)`
     updateProblem(equation, answer);
 }
 
-//chooses and calls from a list of functions that create two step equations
+// Randomly chooses from a list of function that create different
+// Types of two-step equations. Calls the chosen function.
 function createTwoStep() {
-    document.body.style.backgroundImage = "linear-gradient(15deg, #13547a 0%, #80d0c7 100%)";
-    var toggle = randInt(1, 4);
+    let toggle = randInt(1, 4);
     switch (toggle) {
         case 1:
             return twoAddition();
@@ -105,60 +121,86 @@ function createTwoStep() {
 }
 
 function twoAddition() {
-    var a = randInt(2, 10);
-    var b = randInt(-10, 15);
-    var c = randInt(-10, 15);
-    var equation = `\\(${a}x+${b} = ${a*c+b}\\)`;
-    var answer = `\\(x=${c}\\)`;
+    let a = randInt(2, 10);
+    let b = randInt(-10, 15);
+    let c = randInt(-10, 15);
+    let equation = `\\(${a}x+${b} = ${a*c+b}\\)`;
+    let answer = `\\(x=${c}\\)`;
     updateProblem(equation, answer);
 }
 
 
 function twoMultiplication() {
-    var a = randInt(2, 10);
-    var b = randInt(2,10);
-    var c = randInt(-10, 11);
-    var equation = `\\(\\frac{x}{${a}} - ${b} = ${c}\\)`;
-    var answer = `\\(x=${a*(c+b)}\\)`;
+    let a = randInt(2, 10);
+    let b = randInt(2,10);
+    let c = randInt(-10, 11);
+    let equation = `\\(\\frac{x}{${a}} - ${b} = ${c}\\)`;
+    let answer = `\\(x=${a*(c+b)}\\)`;
     updateProblem(equation, answer);
 }
 
 function twoDivision() {
-    var a = randInt(2, 10);
-    var b = randInt(2, 10);
-    var c = randInt(-5, 5);
-    var equation = `\\(${a}(x + ${b}) = ${a*c}\\)`;
-    var answer = `\\(x=${c-b}\\)`;
+    let a = randInt(2, 10);
+    let b = randInt(2, 10);
+    let c = randInt(-5, 5);
+    let equation = `\\(${a}(x + ${b}) = ${a*c}\\)`;
+    let answer = `\\(x=${c-b}\\)`;
     updateProblem(equation, answer);
 }
 
-//chooses and calls from a list of functions that returns multi-step
+// Calls the function that creates a multi-step equation
+// In later development can add more sub functions that will
+// Increase the variety of multi-step equations shown to users
 function createMultiStep() {
     multiDistribute();
-    document.body.style.backgroundImage = "linear-gradient(to top, #0ba360 0%, #3cba92 100%)";
 }
 
 function multiDistribute() {
-    var a = randInt(2, 11);
-    var b = randInt(2, 6);
-    var c = randInt(2, 6);
-    var d = randInt(-10, 10)
-    var equation = `\\(${a}(${b}x+${c}) = ${d}\\)`;
+    let a = randInt(2, 11);
+    let b = randInt(2, 6);
+    let c = randInt(2, 6);
+    let d = randInt(-10, 10)
+    let equation = `\\(${a}(${b}x+${c}) = ${d}\\)`;
 
-    var numerator = d-a*c;
-    var denominator = a*b;
+    let numerator = d-a*c;
+    let denominator = a*b;
 
-    var answer = simplifyFraction(numerator, denominator);
+    let answer = simplifyFraction(numerator, denominator);
     updateProblem(equation, answer);
 }
 
-function gcd_two_numbers(x, y) {
+function createVobs() {
+    let a = randInt(2, 11);
+    let b = randInt(2, 6);
+    let c = randInt(2, 11);
+    let d = randInt(2, 6);
+    let equation = `\\(${a}x+${b}=${c}x+${d}\\)`;
+    
+    if (a == c && b == d) {
+        var answer = `\\(x=\\mathbb{R}\\)`;
+    } else if (a == c) {
+        var answer = 'No Solution';
+    } else {
+        var numerator = d-b;
+        var denominator = a-c;
+        var answer = simplifyFraction(numerator, denominator);
+    }
+    updateProblem(equation, answer);
+}
+
+function randInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+  function gcd_two_numbers(x, y) {
     if ((typeof x !== 'number') || (typeof y !== 'number')) 
       return false;
     x = Math.abs(x);
     y = Math.abs(y);
     while(y) {
-      var t = y;
+      let t = y;
       y = x % y;
       x = t;
     }
@@ -167,7 +209,7 @@ function gcd_two_numbers(x, y) {
 
   function simplifyFraction(numerator, denominator) {
     
-    var gcd = gcd_two_numbers(numerator, denominator);
+    let gcd = gcd_two_numbers(numerator, denominator);
     numerator = numerator/gcd;
     denominator = denominator/gcd;
 
@@ -176,7 +218,7 @@ function gcd_two_numbers(x, y) {
         denominator = denominator *-1;
     }
 
-    var isNegative = 0;
+    let isNegative = 0;
 
     if (numerator < 0) {
         numerator = numerator *-1;
@@ -198,42 +240,10 @@ function gcd_two_numbers(x, y) {
     }
   }
 
-function createVobs() {
-    var a = randInt(2, 11);
-    var b = randInt(2, 6);
-    var c = randInt(2, 11);
-    var d = randInt(2, 6);
-    var equation = `\\(${a}x+${b}=${c}x+${d}\\)`;
-    
-    if (a == c && b == d) {
-        var answer = `\\(x=\\mathbb{R}\\)`;
-    } else if (a == c) {
-        var answer = 'No Solution';
-    } else {
-        var numerator = d-b;
-        var denominator = a-c;
-        var answer = simplifyFraction(numerator, denominator);
-    }
-    updateProblem(equation, answer);
-    document.body.style.backgroundImage = "linear-gradient(45deg, #874da2 0%, #c43a30 100%)";
-}
-
-function randInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-
-  //not called
-  function updateBackground() {
-      let colors = ["linear-gradient(to top, #007adf 0%, #00ecbc 100%)", "linear-gradient( 109.6deg,  rgba(62,161,219,1) 11.2%, rgba(93,52,236,1) 100.2% )", "linear-gradient(to top, #30cfd0 0%, #330867 100%)", "linear-gradient(45deg, #874da2 0%, #c43a30 100%)", "linear-gradient(to top, #0ba360 0%, #3cba92 100%)", "linear-gradient(15deg, #13547a 0%, #80d0c7 100%)", "linear-gradient(to right, #1f4037, #99f2c8)", "linear-gradient( 135deg, #FF9D6C 10%, #BB4E75 100%)"];
-      let i = randInt(0, colors.length - 1);
-      document.body.style.backgroundImage = colors[i];
-  }
-
+  // Opens the menu with animation
   function openMenu() {
-      document.getElementById('menu').style.display = "flex";
-      menuIcon = document.getElementById('menu-icon');
+      document.getElementById('menu').classList.remove('closed');
+      let menuIcon = document.getElementById('menu-icon');
       menuIcon.style.visibility = "hidden";
       menuIcon.classList.add('paused');
       menuIcon.classList.remove("blur-focus");
@@ -242,25 +252,18 @@ function randInt(min, max) {
       document.getElementsByClassName('speed-toggle')[0].style.filter = "opacity(.25)";
   }
 
-
-//   This function is not working!! Want to reverse animation on leave
+// Closes the menu with animation
   function closeMenu() {
-      document.getElementById('menu').style.display = "none";
-      menuIcon = document.getElementById('menu-icon');
-      menuIcon.style.visibility = "visible";
+      document.getElementById('menu').classList.add('closed');
+      let menuIcon = document.getElementById('menu-icon');
+      visibilityTimer = setTimeout( function() {
+          menuIcon.style.visibility = "visible";
+      }, 300);
       menuIcon.classList.add('blur-focus');
       document.getElementsByClassName('center-box')[0].style.filter = "opacity(1)";
       document.getElementsByClassName('speed-outer')[0].style.filter = "opacity(1)";
       document.getElementsByClassName('speed-toggle')[0].style.filter = "opacity(1)";
   }
-
-  function toggleMenu() {
-    if (document.getElementById('menu').style.display == "none") {
-        openMenu();
-    } else {
-        closeMenu();
-    }
-}
 
 function showAnswer() {
     document.getElementById("current-answer").style.opacity = "1";
@@ -272,10 +275,8 @@ function hideAnswer() {
 
 function timeAnswer() {
     clearTimeout(window.answerTimer);
-    
-    var svg = document.getElementsByClassName("speed-toggle")[0];
-    var currentSpeed = svg.id;
-
+    let svg = document.getElementsByClassName("speed-toggle")[0];
+    let currentSpeed = svg.id;
     if (currentSpeed == "slow") {
         var speed = 45000; //slowest speed
     } else if (currentSpeed == "medium") {
@@ -289,10 +290,11 @@ function timeAnswer() {
     }, speed);
 }
 
+// Handles updating the ids and animation when a user changes the speed
 function changeSpeed() {
-    var svg = document.getElementsByClassName("speed-toggle")[0];
-    var speed = svg.id;
-    var info = document.getElementsByClassName("speed-info")[0];
+    let svg = document.getElementsByClassName("speed-toggle")[0];
+    let speed = svg.id;
+    let info = document.getElementsByClassName("speed-info")[0];
 
     if (speed == "slow") {
         // update to medium
@@ -324,4 +326,5 @@ function hideSpeedInfo() {
     }, 5000);
 }
 
+// Called to show the default speed but hide 5s after page load
 hideSpeedInfo();
